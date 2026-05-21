66c351ae4d153a372b3e06073b9397ff418d1601
ayane1118 <183798921+ayane1118@users.noreply.github.com>
2026-05-22 00:31:05 +0900

    Reactのページ構成

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import './App.css'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Features from './components/Features'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="app">
-      <h1>Awake</h1>
+    <div>
+      <Header />
+      <Hero />
+      <Features />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Features.tsx
@@ -0,0 +1,9 @@
+function Features() {
+  return (
+    <section>
+      <h2>機能紹介</h2>
+    </section>
+  )
+}
+
+export default Features
