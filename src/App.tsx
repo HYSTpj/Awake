e21a4e3ad6715b5e987473b094ec9698bca28560
ayane1118 <183798921+ayane1118@users.noreply.github.com>
2026-05-25 22:57:48 +0900

    Vercelエラー修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Header from './components/Header'
-import Hero from './components/Hero'
-import TechStack from './components/TechStack'
+import Hero from './components/Hero/Hero'
+import TechStack from './components/TechStack/TechStack'
 import Features from './components/Features'
 import Footer from './components/Footer'
 
