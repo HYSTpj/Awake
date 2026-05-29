fc6564ad801491f3c6fb8e1c69e37ca0822ed4fc
ayane1118 <183798921+ayane1118@users.noreply.github.com>
2026-05-29 11:57:50 +0900

    テーム紹介追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Header from './components/Header'
 import Hero from './components/Hero/Hero'
 import TechStack from './components/TechStack/TechStack'
+import Team from './components/Team/Team'
 import Profile from './components/Profile/Profile'
 import Features from './components/Features'
 import Footer from './components/Footer'
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Hero />
       <TechStack />
+      <Team />
       <Profile />
       <Features />
       <Footer />
